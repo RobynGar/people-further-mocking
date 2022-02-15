e4e963e49fd82bc332d1ad7fd3f8d02da51a855b
Robyn Garlington <robyn.garlington@hotmail.co.uk>
2022-02-15 16:57:02 +0000

    created override methods for the DAO interface

--- src/main/java/com/robyn/peopleproject/PeopleProjectApplication.java
+++ src/main/java/com/robyn/peopleproject/PeopleProjectApplication.java
@@ -8,6 +8,11 @@ public class PeopleProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PeopleProjectApplication.class, args);
+	Person colin = new Person(1, "colin", 4);
+	PersonDAO personDAO = new PersonDataAccessService();
+	PersonService personService = new PersonService(personDAO);
+	personService.savePerson(colin);
+
 	}
 
 }
--- src/main/java/com/robyn/peopleproject/PersonDataAccessService.java
+++ src/main/java/com/robyn/peopleproject/PersonDataAccessService.java
@@ -1,26 +1,45 @@
 package com.robyn.peopleproject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PersonDataAccessService implements PersonDAO {
+    //initialise outside the constructor so that in can be
+    // accessed everywhere in the class
+    List<Person> people;
+
+    // constructor
+    public PersonDataAccessService() {
+        //initialise this property by assigning it to this
+        // array list
+        this.people = new ArrayList<>();
+    }
 
     @Override
     public void savePerson(Person person) {
-        
+       // this. used to specify that we mean the person property
+        // in this class. To make the distinction between
+        // which person we mean as person is also passed in as an argument above
+        this.people.add(person);
     }
 
     @Override
     public void deletePerson(int id) {
-
+        this.people.remove(getPersonById(id));
     }
 
     @Override
     public List<Person> getPeople() {
-        return null;
+        return people;
     }
 
     @Override
     public Person getPersonById(int id) {
+        for(Person p : people) {
+            if(id == p.getId()){
+                return p;
+            }
+        }
         return null;
     }
 }
